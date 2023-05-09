368d27e7eb49cad2296f162c4af04c2dec4ae607
Tony Holdstock-Brown <tonyhb@gmail.com>
2023-05-09 14:09:55 -0700

    remove env key

--- api/src/lib/inngest.ts
+++ api/src/lib/inngest.ts
@@ -1,6 +1,6 @@
 import { Inngest } from 'inngest'
 
-export const INNGEST_APP_NAME = 'Redwood_Inngest'
+export const INNGEST_APP_NAME = 'Test app'
 
 export const inngest = new Inngest({
   /**
@@ -8,11 +8,4 @@ export const inngest = new Inngest({
    * resides in.
    */
   name: INNGEST_APP_NAME,
-  /**
-   * Inngest event key, used to send events to Inngest Cloud. If not provided,
-   * will search for the `INNGEST_EVENT_KEY` environment variable. If neither
-   * can be found, however, a warning will be shown and any attempts to send
-   * events will throw an error.
-   */
-  eventKey: 'YOUR_INNGEST_EVENT_KEY',
 })
