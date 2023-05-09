ce1824363d770b0e65faa8f8cd259606993c92b7
Tony Holdstock-Brown <tonyhb@gmail.com>
2023-05-09 14:46:37 -0700

    show env on 404

--- web/src/pages/NotFoundPage/NotFoundPage.tsx
+++ web/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -40,7 +40,7 @@ export default () => (
     <section>
       <h1>
         <span>404 Page Not Found</span>
-        <span>{env}</span>
+        <span>env: "{env}"</span>
       </h1>
     </section>
   </main>
