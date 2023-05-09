af992842371986c87a969bbb7b2b9e0297a1520e
Tony Holdstock-Brown <tonyhb@gmail.com>
2023-05-09 14:36:07 -0700

    Clean client

--- api/src/functions/inngest.ts
+++ api/src/functions/inngest.ts
@@ -16,23 +16,4 @@ export const handler = serve(inngest, inngestFunctions, {
    * Default level: "info"
    */
   logLevel: 'info',
-  /**
-   * A key used to sign requests to and from Inngest in order to prove that the
-   * source is legitimate in Production.
-   *
-   * You must provide a signing key to communicate securely with Inngest. If
-   * your key is not provided here, we'll try to retrieve it from the
-   * `INNGEST_SIGNING_KEY` environment variable.
-   *
-   * You can retrieve your signing key from the Inngest UI inside the "Secrets"
-   * section at {@link https://app.inngest.com/env/production/manage/signing-key}. We highly recommend
-   * that you add this to your platform's available environment variables as
-   * `INNGEST_SIGNING_KEY`.
-   *
-   * If no key can be found, you will not be able to register your functions or
-   * receive events from Inngest.
-   *
-   * Note: This is not required for local development.
-   */
-  // signingKey: 'YOUR-SIGNING-KEY',
 })
